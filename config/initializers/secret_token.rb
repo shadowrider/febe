fe244ee3d3708cfc7937fc93dc7aa3f4506d150b
Myroslav Pomazan <ShadowRider@Myroslav-Pomazans-MacBook-Pro.local>
2011-11-13 11:38:05 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Febe::Application.config.secret_token = '97a978cca79be8d54fb600b4331432f346bdfe502047c4a204634f2a8a4d3518d67da6779b5cd2ccd6821fddd10ad09ca43d825878ed8ba905ecc8909eb64961'
